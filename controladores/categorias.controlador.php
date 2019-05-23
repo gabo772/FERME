0005be45e851c3630320946e8f542d097aaae224
Gabriel Ramirez <gramirez@saltala.com>
2019-05-23 01:20:26 -0400

    proyecto

---
+++ controladores/categorias.controlador.php
@@ -0,0 +1,181 @@
+<?php
+
+class ControladorCategorias{
+
+	/*=============================================
+	CREAR CATEGORIAS
+	=============================================*/
+
+	static public function ctrCrearCategoria(){
+
+		if(isset($_POST["nuevaCategoria"])){
+
+			if(preg_match('/^[a-zA-Z0-9ñÑáéíóúÁÉÍÓÚ ]+$/', $_POST["nuevaCategoria"])){
+
+				$tabla = "familias";
+
+				$datos = $_POST["nuevaCategoria"];
+
+				$respuesta = ModeloCategorias::mdlIngresarCategoria($tabla, $datos);
+
+				if($respuesta == "ok"){
+
+					echo'<script>
+
+					swal({
+						  type: "success",
+						  title: "La familia ha sido guardada correctamente",
+						  showConfirmButton: true,
+						  confirmButtonText: "Cerrar"
+						  }).then(function(result){
+									if (result.value) {
+
+									window.location = "categorias";
+
+									}
+								})
+
+					</script>';
+
+				}
+
+
+			}else{
+
+				echo'<script>
+
+					swal({
+						  type: "error",
+						  title: "¡La familia no puede ir vacía o llevar caracteres especiales!",
+						  showConfirmButton: true,
+						  confirmButtonText: "Cerrar"
+						  }).then(function(result){
+							if (result.value) {
+
+							window.location = "categorias";
+
+							}
+						})
+
+			  	</script>';
+
+			}
+
+		}
+
+	}
+
+	/*=============================================
+	MOSTRAR CATEGORIAS
+	=============================================*/
+
+	static public function ctrMostrarCategorias($item, $valor){
+
+		$tabla = "familias";
+
+		$respuesta = ModeloCategorias::mdlMostrarCategorias($tabla, $item, $valor);
+
+		return $respuesta;
+	
+	}
+
+	/*=============================================
+	EDITAR CATEGORIA
+	=============================================*/
+
+	static public function ctrEditarCategoria(){
+
+		if(isset($_POST["editarCategoria"])){
+
+			if(preg_match('/^[a-zA-Z0-9ñÑáéíóúÁÉÍÓÚ ]+$/', $_POST["editarCategoria"])){
+
+				$tabla = "familias";
+
+				$datos = array("categoria"=>$_POST["editarCategoria"],
+							   "id"=>$_POST["idCategoria"]);
+
+				$respuesta = ModeloCategorias::mdlEditarCategoria($tabla, $datos);
+
+				if($respuesta == "ok"){
+
+					echo'<script>
+
+					swal({
+						  type: "success",
+						  title: "La familia ha sido cambiada correctamente",
+						  showConfirmButton: true,
+						  confirmButtonText: "Cerrar"
+						  }).then(function(result){
+									if (result.value) {
+
+									window.location = "categorias";
+
+									}
+								})
+
+					</script>';
+
+				}
+
+
+			}else{
+
+				echo'<script>
+
+					swal({
+						  type: "error",
+						  title: "¡La familia no puede ir vacía o llevar caracteres especiales!",
+						  showConfirmButton: true,
+						  confirmButtonText: "Cerrar"
+						  }).then(function(result){
+							if (result.value) {
+
+							window.location = "categorias";
+
+							}
+						})
+
+			  	</script>';
+
+			}
+
+		}
+
+	}
+
+	/*=============================================
+	BORRAR CATEGORIA
+	=============================================*/
+
+	static public function ctrBorrarCategoria(){
+
+		if(isset($_GET["idCategoria"])){
+
+			$tabla ="familias";
+			$datos = $_GET["idCategoria"];
+
+			$respuesta = ModeloCategorias::mdlBorrarCategoria($tabla, $datos);
+
+			if($respuesta == "ok"){
+
+				echo'<script>
+
+					swal({
+						  type: "success",
+						  title: "La familia ha sido borrada correctamente",
+						  showConfirmButton: true,
+						  confirmButtonText: "Cerrar"
+						  }).then(function(result){
+									if (result.value) {
+
+									window.location = "categorias";
+
+									}
+								})
+
+					</script>';
+			}
+		}
+		
+	}
+}
---
+++ vistas/modulos/footer.php
@@ -0,0 +1,9 @@
+<footer class="main-footer">
+	
+	<strong>Copyright &copy; 2019 <a href="https://www.google.com" target="_blank">Google</a>.
+	</strong>
+
+	Todos los derechos reservados
+
+
+</footer>
